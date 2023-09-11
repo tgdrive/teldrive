5f3552c8fe66db5f578cabdd2f01a8237a36749a
divyam234 <divyam@mnnit.ac.in>
2023-09-11 13:32:32 +0530

    changes related to upload check

--- database/migrations/20230911113331_alter_table.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-
-ALTER TABLE teldrive.files DROP CONSTRAINT unique_file;
-
-ALTER TABLE teldrive.files ADD CONSTRAINT unique_file UNIQUE (name, parent_id, user_id,status)
-
--- +goose StatementEnd
--- services/file.service.go
+++ services/file.service.go
@@ -195,7 +195,10 @@ func (fs *FileService) ListFiles(c *gin.Context) (*schemas.FileResponse, *types.
 			delete(filterQuery, "updated_at")
 		}
 
-		setOrderFilter(query, &pagingParams, &sortingParams)
+		if filterQuery["path"] != "" && filterQuery["name"] != "" {
+			query.Where("parent_id in (?)", fs.Db.Model(&models.File{}).Select("id").Where("path = ?", filterQuery["path"]))
+			delete(filterQuery, "path")
+		}
 
 		query.Order("type DESC").Order(getOrder(sortingParams)).Where(filterQuery)
 
