fa51e2f0f2ea052d2aba5fc3c3e3c20b3c8e4e73
divyam234 <47589864+divyam234@users.noreply.github.com>
2023-11-25 10:36:20 +0530

    update ui ref

--- utils/recovery/recovery.go
+++ utils/recovery/recovery.go
@@ -2,16 +2,13 @@ package recovery
 
 import (
 	"context"
-	"time"
 
 	"github.com/cenkalti/backoff/v4"
-	"github.com/divyam234/teldrive/utils"
 	"github.com/go-faster/errors"
 	"github.com/gotd/td/bin"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/tg"
 	"github.com/gotd/td/tgerr"
-	"go.uber.org/zap"
 )
 
 type recovery struct {
@@ -39,9 +36,7 @@ func (r *recovery) Handle(next tg.Invoker) telegram.InvokeFunc {
 			}
 
 			return nil
-		}, r.backoff, func(err error, duration time.Duration) {
-			utils.Logger.Debug("Wait for connection recovery", zap.Error(err), zap.Duration("duration", duration))
-		})
+		}, r.backoff, nil)
 	}
 }
 
