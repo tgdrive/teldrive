59a0702df2002e5519ec9f27459b834f9441035f
Mohd Hassan Khan <149296407+hassankhan2608@users.noreply.github.com>
2025-08-15 18:21:29 +0530

    feat: add HEIF, HEIC, and WebP to  image extensions
    - Added some image formats: heif, heic, webp

--- internal/category/category.go
+++ internal/category/category.go
@@ -18,7 +18,7 @@ const (
 
 var (
 	documentExtensions = []string{"doc", "docx", "ppt", "pptx", "pps", "ppsx", "odt", "xls", "xlsx", "csv", "pdf", "txt"}
-	imageExtensions    = []string{"jpg", "jpeg", "png", "gif", "bmp", "svg"}
+	imageExtensions    = []string{"jpg", "jpeg", "png", "gif", "bmp", "svg", "webp", "heif", "heic"}
 	videoExtensions    = []string{"mp4", "webm", "mov", "avi", "m4v", "flv", "wmv", "mkv", "mpg", "mpeg", "m2v", "mpv"}
 	audioExtensions    = []string{"mp3", "wav", "ogg", "m4a", "flac", "aac", "wma", "aiff", "ape", "alac", "opus", "pcm"}
 	archiveExtensions  = []string{"zip", "rar", "tar", "gz", "7z", "iso", "dmg", "pkg"}
